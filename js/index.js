05076b763ff30085aac1440210902c7e765c15b3
Krishnendu Nandy <krishnendunandy6@gmail.com>
2022-04-06 10:22:56 +0530

    branch-1 #comment introduction and syntax of JavaScript

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function viewDate(){
+    document.write(Date());  //It is only for testing purpose, when we use it it will replace all the html code
+    console.log(Date());
+    alert(Date());
+}
