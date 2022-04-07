228e3664b0fb9004e578ceb2cbdb6efe6796448a
Krishnendu Nandy <krishnendunandy6@gmail.com>
2022-04-07 09:11:00 +0530

    Variables in java let,const,var

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 function viewDate(){
     document.write(Date());  //It is only for testing purpose, when we use it it will replace all the html code
-    console.log(Date());
-    alert(Date());
+    console.log(Date()); //in console
+    alert(Date()); //alert
 }
