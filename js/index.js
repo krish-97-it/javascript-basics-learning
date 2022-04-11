8b245d653d8260ee9f8d3ed742b8d45e5c6cc92b
Krishnendu Nandy <krishnendunandy6@gmail.com>
2022-04-11 13:49:38 +0530

    branch-8 #commit calling function inside javascript

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-function viewDate(){
+var datebtn = document.getElementById("viewbtn");
+
+datebtn.onclick = function display(){
     document.write(Date());  //It is only for testing purpose, when we use it it will replace all the html code
     console.log(Date()); //in console
     alert(Date()); //alert
